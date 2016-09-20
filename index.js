c17972e2d5170963b2025ba9fe3e8d7e973325db
Yuval Saraf <unimonkiez@gmail.com>
2016-09-20 14:12:43 +0300

    Changed class to output only one fontFamily

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const replaceCssContents = require('./replace-css-contents');
 
 module.exports = class IconMaker {
   constructor({
-    files = ['ttf', 'woff', 'eot', 'svg']
+    files = ['ttf', 'woff', 'eot', 'svg'],
+    fontFamily = 'default'
   } = {}) {
     if (files === undefined || !Array.isArray(files)) {
       throw new Error(`\`files\` must be an array, got ${typeof files}.`);
@@ -15,15 +16,12 @@ module.exports = class IconMaker {
     if (files.indexOf('ttf') === -1) {
       throw new Error('`files` must contain `ttf`.');
     }
+    this._fontFamily = fontFamily;
     this._files = files;
-    this._fonts = {};
+    this._svgs = [];
   }
   addSvg(svgPath, fontFamily = 'default') {
-    const fonts = this._fonts;
-    if (fonts[fontFamily] === undefined) {
-      fonts[fontFamily] = [];
-    }
-    fonts[fontFamily].push(svgPath);
+    this._svgs.push(svgPath);
   }
   run(cb) {
     if (typeof cb !== 'function') {
@@ -31,52 +29,43 @@ module.exports = class IconMaker {
     }
 
     const files = this._files;
-    const fontsPromises = Object.keys(this._fonts).map(fontFamily => new Promise((resolve, reject) => {
-      const svgPaths = this._fonts[fontFamily];
+    const svgPaths = this._svgs;
+    const fontFamily = this._fontFamily;
 
-      let fontmin = new Fontmin().src(svgPaths).use(Fontmin.svgs2ttf(fontFamily));
-      if (files.indexOf('eot') !== -1) {
-        fontmin = fontmin.use(Fontmin.ttf2eot({ fontFamily }));
-      }
-      if (files.indexOf('woff') !== -1) {
-        fontmin = fontmin.use(Fontmin.ttf2woff({ fontFamily }));
-      }
-      if (files.indexOf('svg') !== -1) {
-        fontmin = fontmin.use(Fontmin.ttf2svg({ fontFamily }));
-      }
-      fontmin.use(Fontmin.css({
-        fontFamily,
-        iconPrefix: fontFamily,
-        glyph: true
-      }));
-
-      fontmin.run((err, fontFiles) => {
-        if (err) {
-          reject(err);
-        }
-        resolve(
-          fontFiles.reduce((obj, fontFile) => {
-            if (path.extname(fontFile.path) === '.css') {
-              const cssContents = replaceCssContents(fontFile.contents.toString(), files);
-
-              return Object.assign(obj, {
-                css: cssContents
-              });
-            } else {
-              return Object.assign(obj, {
-                fontFiles: (obj.fontFiles || [])
-                .concat(fontFile)
-              });
-            }
-          }, { fontFamily })
-        );
-      });
+    let fontmin = new Fontmin().src(svgPaths).use(Fontmin.svgs2ttf(fontFamily));
+    if (files.indexOf('eot') !== -1) {
+      fontmin = fontmin.use(Fontmin.ttf2eot({ fontFamily }));
+    }
+    if (files.indexOf('woff') !== -1) {
+      fontmin = fontmin.use(Fontmin.ttf2woff({ fontFamily }));
+    }
+    if (files.indexOf('svg') !== -1) {
+      fontmin = fontmin.use(Fontmin.ttf2svg({ fontFamily }));
+    }
+    fontmin.use(Fontmin.css({
+      fontFamily,
+      iconPrefix: fontFamily,
+      glyph: true
     }));
 
-    Promise.all(fontsPromises).then(fonts => {
-      cb(undefined, fonts);
-    }, err => {
-      cb(err);
+    fontmin.run((err, fontFiles) => {
+      if (err) {
+        cb(err);
+      }
+      cb(undefined, fontFiles.reduce((obj, fontFile) => {
+        if (path.extname(fontFile.path) === '.css') {
+          const cssContents = replaceCssContents(fontFile.contents.toString(), files);
+
+          return Object.assign(obj, {
+            css: cssContents
+          });
+        } else {
+          return Object.assign(obj, {
+            fontFiles: (obj.fontFiles || [])
+            .concat(fontFile)
+          });
+        }
+      }, { fontFamily }));
     });
   }
 };
--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ const IconMaker = require(path.join(__dirname, '..', 'index.js'));
 
 const iconMaker = new IconMaker({ files: ['ttf', 'woff'] });
 iconMaker.addSvg(path.join(__dirname, 'svg', 'yin-yan.svg'));
-iconMaker.addSvg(path.join(__dirname, 'svg', 'yin-yan.svg'), 'blaEE');
-iconMaker.run((err, fonts) => {
-  console.log(fonts[0].fontFiles[0].path);
+iconMaker.addSvg(path.join(__dirname, 'svg', 'yin-yan.svg'));
+iconMaker.run((err, font) => {
+  console.log(font.fontFiles[0].path);
 });
