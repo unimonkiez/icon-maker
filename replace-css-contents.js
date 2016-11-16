588c6e04255101e3e5e9c7aca3342b70d149544e
Yuval Saraf <unimonkiez@gmail.com>
2016-11-16 21:07:43 +0200

    No need for vertical align in before

--- replace-css-contents.js
+++ replace-css-contents.js
@@ -1,2 +1,3 @@
 module.exports = (cssContents, isLocalCss) => cssContents
+  .replace(/vertical\-align: top;\n/g, '')
   .replace(isLocalCss ? /(\..*?){/g : '', isLocalCss ? ((_, w) => `:local(${w.trim()}) {`) : ''); // Change class to locale class
